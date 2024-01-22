552cb238f131370e1365a72bca9044872b44c453
SaniaFatima123 <fatimasania648@gmail.com>
2024-01-22 23:19:43 +0530

    backend jwt token in progress

---
+++ backend/demo/src/main/java/com/project/demo/service/IRoleService.java
@@ -0,0 +1,7 @@
+package com.project.demo.service;
+
+import com.project.demo.entity.Role;
+
+public interface IRoleService {
+    Role createUser(Role role);
+}
---
+++ backend/demo/src/main/java/com/project/demo/service/RoleServiceImpl.java
@@ -0,0 +1,18 @@
+package com.project.demo.service;
+
+import com.project.demo.dao.RoleDao;
+import com.project.demo.entity.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements IRoleService{
+    @Autowired
+    private RoleDao dao;
+
+    @Override
+    public Role createUser(Role role) {
+       return dao.save(role);
+    }
+}
